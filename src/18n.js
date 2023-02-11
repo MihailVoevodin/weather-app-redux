34816fb1e23c55687932c90dbca8c41242dce766
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-02-11 15:21:41 +0300

    fix deploy version for translation and fix scroll jump

--- src/18n.js
+++ src/18n.js
@@ -3,11 +3,106 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import {initReactI18next} from 'react-i18next';
 
+const translationEN = {
+    searchPanel: {
+        header: 'Check the Weather',
+        searchPanelPlaceholder: 'Input city',
+        errorMessage: 'No matching location found',
+    },
+    currentWeather: {
+        locationcity: '{{city}}',
+        feelsLike: 'feels like',
+        condition: '{{condition}}',
+    },
+    currentWeatherDetails: {
+        humidity: 'Humidity',
+        wind: 'Wind speed',
+        windDirection: 'Wind dir',
+        pressure: 'Pressure',
+        pressureUnits: 'mmHg',
+    },
+    forecastWeather: {
+        details: 'Details',
+        astronomy: 'Astronomy',
+        hourlyForecast: 'Hourly forecast',
+        Details: {
+            avgTemperature: 'Avg temperature',
+            maxWindSpeed: 'Max wind speed',
+            totalPrecipitation: 'Total precipitation',
+            avgVisibility: 'Avg visibility',
+            avgHumidity: 'Avg humidity',
+            chanceOfRain: 'Chance of rain',
+            chanceOfSnow: 'Chance of snow',
+        },
+        Astro: {
+            sunrise: 'Sunrise',
+            sunset: 'Sunset',
+            dayLength: 'Day length',
+        },
+    },
+    weatherUnits: {
+        'm/s': 'm/s',
+        mmHg: 'mmHg',
+        mm: 'mm',
+        km: 'km',
+    },
+};
+
+const translationRU = {
+    searchPanel: {
+        header: 'Проверь погоду',
+        searchPanelPlaceholder: 'Введите город',
+        errorMessage: 'Город не найден',
+    },
+    currentWeather: {
+        locationcity: '{{city}}',
+        feelsLike: 'Ощущается как',
+        condition: '{{condition}}',
+    },
+    currentWeatherDetails: {
+        humidity: 'Влажность',
+        wind: 'Скорость ветра',
+        windSpeedUnits: 'м/с',
+        windDirection: 'Напр. ветра',
+        pressure: 'Давление',
+        pressureUnits: 'мм рт. ст.',
+    },
+    forecastWeather: {
+        details: 'Детали',
+        astronomy: 'Астрономия',
+        hourlyForecast: 'Почасовой прогноз',
+        Details: {
+            avgTemperature: 'Ср. температура',
+            maxWindSpeed: 'Макс. скор. ветра',
+            totalPrecipitation: 'Кол-во осадков',
+            avgVisibility: 'Ср. видимость',
+            avgHumidity: 'Ср. влажность',
+            chanceOfRain: 'Шанс дождя',
+            chanceOfSnow: 'Шанс снега',
+        },
+        Astro: {
+            sunrise: 'Восход',
+            sunset: 'Закат',
+            dayLength: 'Световой день',
+        },
+    },
+    weatherUnits: {
+        'm/s': 'м/с',
+        mmHg: 'мм рт. ст.',
+        mm: 'мм',
+        km: 'км',
+    },
+};
+
 void i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        resources: {
+            en: {translation: translationEN},
+            ru: {translation: translationRU},
+        },
         fallbackLng: 'en',
         debug: true,
         detection: {
@@ -15,7 +110,7 @@ void i18n
             cache: ['cookie'],
         },
         interpolation: {
-            escapeValue: false,
+            escapeValue: true,
         },
     });
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
+import Spinner from 'Common/Components/Spinner';
 import {store} from 'Redux/Store';
 import './index.css';
 import App from './App';
@@ -11,7 +12,7 @@ import './18n';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <Provider store={store}>
-        <Suspense>
+        <Suspense fallback={<Spinner />}>
             <App />
         </Suspense>
     </Provider>
